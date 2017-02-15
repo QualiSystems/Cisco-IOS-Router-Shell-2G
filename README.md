2423110f4caa9d42a418ffc7729596c169a0ffff
alexquali <alexey.b@quali.com>
2017-02-15 16:03:45 +0200

    Cisco IOS Router Shell with shellfoundry structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cisco-IOS-Router-Shell
+CloudShell Networking Cisco IOS Shell for Router
---
+++ tests/test_ciscoiosshell.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for `CiscoiosshellDriver`
+"""
+
+import unittest
+
+from driver import CiscoiosshellDriver
+
+
+class TestCiscoiosshellDriver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
